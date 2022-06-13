aca08fa456faedb756244c6324c77a3ac44d5718
YoloNik <nikitaslipachuk@gmail.com>
2022-06-13 17:04:55 +0300

    my library html, scss
    compitte "my library" html and scss for phones, tablets and laptops

--- src/index.js
+++ src/index.js
@@ -2,26 +2,31 @@ import apiService from './js/fetchApi';
 import fetchNormaMovie from './js/markup-movie-card';
 
 const searchQuery = document.getElementById(`search__form`);
-const errorText = document.querySelector(`.search__error`);
 const homeBtn = document.querySelector('.header__home-btn');
+const myLibrary = document.querySelector('.header__library-btn');
 const movieGallery = document.querySelector(`.gallery`);
 
 searchQuery.addEventListener(`click`, onSearchBtn);
 homeBtn.addEventListener(`click`, onHomeBtn);
+myLibrary.addEventListener(`click`, onLibraryBtn);
 
 fetchNormaMovie();
 
-function onHomeBtn(e) {
-  document.location = `../index.html`;
-}
 function onSearchBtn(e) {
-  //errorText.style.visibility = `hidden`;
   if (e.target.closest(`button`)) {
     e.preventDefault();
     apiService.query = e.currentTarget.elements[0].value;
-    movieGallery.innerHTML = fetchNormaMovie();
+    fetchNormaMovie();
   }
-  //if (movieGallery.childElementCount === 0) {
-  //  errorText.style.visibility = `visible`;
-  //}
+}
+
+function onHomeBtn(e) {
+  homeBtn.classList.add('header__active-page');
+  myLibrary.classList.remove('header__active-page');
+  //document.location = `../index.html`;
+}
+
+function onLibraryBtn(e) {
+  homeBtn.classList.remove('header__active-page');
+  myLibrary.classList.add('header__active-page');
 }
