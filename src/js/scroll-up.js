15691767c5c0a4eb446401ef952a2d436931127f
YoloNik <nikitaslipachuk@gmail.com>
2022-06-24 21:58:31 +0300

    Update scroll-up.js

--- src/js/scroll-up.js
+++ src/js/scroll-up.js
@@ -1,6 +1,6 @@
-const gallery = document.querySelector('.gallery');
-gallery.addEventListener('click', scrollUp);
+//const gallery = document.querySelector('.gallery');
+//gallery.addEventListener('click', scrollUp);
 
-export default function scrollUp(e) {
-  window.scroll(0, 0);
-}
+//export default function scrollUp(e) {
+//  window.scroll(0, 0);
+//}
